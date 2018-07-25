5798435e7b9f78840a4b8887eb6a03186e91e196
jadeloveland <jloveland93@gmail.com>
2018-07-25 15:10:51 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -35,14 +35,14 @@ end
 # first_student_by_method
 
 def last_student_by_method
-puts STUDENT_NAMES.last
+return STUDENT_NAMES.last
 end
 
 # last_student_by_method
 
 def first_second_and_third_students
 room = ["Adele", "Beyoncé", "Cardi B", "Lady Gaga", "Nicki Minaj", "Rihanna"]
-puts room [0..2]
+return room [0..2]
 end
 
 first_second_and_third_students
