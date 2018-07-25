98fe32c7e4804d81b0d408df6b408a4e2af93809
jadeloveland <jloveland93@gmail.com>
2018-07-25 14:59:41 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -8,8 +8,11 @@ STUDENT_NAMES = [
 ]
 
 def first_student_by_index
-room = ["Adele", "Beyoncé", "Cardi B", ]
+room = ["Adele", "Beyoncé", "Cardi B", "Lady Gaga", "Nicki Minaj", "Rihanna"]
+ puts room[0]
 end
+
+first_student_by_index
 
 def fourth_student_by_index
   # Write a solution that returns the first student in the array student_names
