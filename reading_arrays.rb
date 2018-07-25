e7049c7a981c9b2eaffaaf99f03d3efbbb9a9584
jadeloveland <jloveland93@gmail.com>
2018-07-25 15:09:48 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -16,15 +16,15 @@ end
 
 def fourth_student_by_index
 room = ["Adele", "Beyoncé", "Cardi B", "Lady Gaga", "Nicki Minaj", "Rihanna"]
-puts room[3]
+return room[3]
 end
 
 # fourth_student_by_index
  
-# def last_student_by_index
-# room = ["Adele", "Beyoncé", "Cardi B", "Lady Gaga", "Nicki Minaj", "Rihanna"]
-# puts room[-1]
-# end
+def last_student_by_index
+room = ["Adele", "Beyoncé", "Cardi B", "Lady Gaga", "Nicki Minaj", "Rihanna"]
+puts room[-1]
+end
 
 # last_student_by_index
 
